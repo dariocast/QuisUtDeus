1f8e45e359fad5f643b527db1dc21722fe288af1
Dario Castellano <dario.castellano@etuitus.it>
2017-11-08 22:00:14 +0100

    Added PDF rendering example

--- app/src/main/java/it/pentagono/app/quisutdeus/PDFActivity.java
+++ app/src/main/java/it/pentagono/app/quisutdeus/PDFActivity.java
@@ -1,22 +1,44 @@
 package it.pentagono.app.quisutdeus;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class PDFActivity extends AppCompatActivity {
+public class PDFActivity extends FragmentActivity {
+
+    private View btnRender;
+    private LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
 
-        Intent mainIntent = getIntent();
+        //find view by id
+        btnRender = (View)findViewById(R.id.btn_render);
+        container = (LinearLayout)findViewById(R.id.fragment_layout);
 
-        WebView webview = (WebView) findViewById(R.id.webview);
-        webview.getSettings().setJavaScriptEnabled(true);
-        String pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
-        webview.loadUrl("http://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
+        //set event handling for button
+        btnRender.setOnClickListener(onClickListener());
+    }
+
+    private View.OnClickListener onClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //replace fragment when clicked
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.fragment_layout, new PDFRenderFragment());
+                ft.commit();
+
+                //gone button after all
+                btnRender.setVisibility(View.GONE);
+                container.setVisibility(View.VISIBLE);
+            }
+        };
     }
 }
